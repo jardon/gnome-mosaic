167bd7f6dce58574f113369c925c2af57394f5ce
Jarred Wilson <jarred.wilson12@gmail.com>
2025-07-26 14:37:48 -0500

    feat:[#49] add active-hint-border-width setting

--- src/settings.ts
+++ src/settings.ts
@@ -63,6 +63,7 @@ function settings_new_schema(schema: string): Settings {
 
 const ACTIVE_HINT = 'active-hint';
 const ACTIVE_HINT_BORDER_RADIUS = 'active-hint-border-radius';
+const ACTIVE_HINT_BORDER_WIDTH = 'active-hint-border-width';
 const GNOME_LEGACY_ACCENT_COLOR = 'gnome-legacy-accent-color';
 const COLUMN_SIZE = 'column-size';
 const EDGE_TILING = 'edge-tiling';
@@ -98,6 +99,10 @@ export class ExtensionSettings {
 
     active_hint_border_radius(): number {
         return this.ext.get_uint(ACTIVE_HINT_BORDER_RADIUS);
+    }
+
+    active_hint_border_width(): number {
+        return this.ext.get_uint(ACTIVE_HINT_BORDER_WIDTH);
     }
 
     gnome_legacy_accent_color(): string {
@@ -197,6 +202,10 @@ export class ExtensionSettings {
 
     set_active_hint_border_radius(set: number) {
         this.ext.set_uint(ACTIVE_HINT_BORDER_RADIUS, set);
+    }
+
+    set_active_hint_border_width(set: number) {
+        this.ext.set_uint(ACTIVE_HINT_BORDER_WIDTH, set);
     }
 
     set_gnome_legacy_accent_color(color: string) {
