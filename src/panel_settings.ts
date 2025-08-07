e237534bde7cc66ea2ece3b7029fd452d6b42048
Jarred Wilson <jarred.wilson12@gmail.com>
2025-08-07 07:49:24 -0500

    refactor: move gap width setting
    - remove gap width setting from indicator
    - add gap width setting to prefs

--- src/panel_settings.ts
+++ src/panel_settings.ts
@@ -27,8 +27,6 @@ export class Indicator {
     toggle_active: any;
     border_radius: any;
 
-    entry_gaps: any;
-
     constructor(ext: Ext) {
         this.button = new Button(0.0, _('GNOME Mosaic Settings'));
 
@@ -70,15 +68,6 @@ export class Indicator {
             }
         );
 
-        this.entry_gaps = number_entry(
-            _('Gaps'),
-            ext.settings.gap_inner(),
-            value => {
-                ext.settings.set_gap_inner(value);
-                ext.settings.set_gap_outer(value);
-            }
-        );
-
         bm.addMenuItem(this.toggle_tiled);
         bm.addMenuItem(floating_window_exceptions(ext, bm));
 
@@ -93,8 +82,6 @@ export class Indicator {
         }
 
         bm.addMenuItem(this.toggle_active);
-
-        bm.addMenuItem(this.entry_gaps);
     }
 
     destroy() {
@@ -216,96 +203,6 @@ function shortcuts(menu: any): any {
     });
 
     return item;
-}
-
-function clamp(input: number, min = 0, max = 128): number {
-    return Math.min(Math.max(min, input), max);
-}
-
-function number_entry(
-    label: string,
-    valueOrOptions: number | {value: number; min: number; max: number},
-    callback: (a: number) => void
-): any {
-    let value = valueOrOptions,
-        min: number,
-        max: number;
-    if (typeof valueOrOptions !== 'number')
-        ({value, min, max} = valueOrOptions);
-
-    const entry = new St.Entry({
-        text: String(value),
-        input_purpose: Clutter.InputContentPurpose.NUMBER,
-        x_align: Clutter.ActorAlign.CENTER,
-        x_expand: false,
-    });
-
-    entry.set_style('width: 5em');
-    entry.connect('button-release-event', () => {
-        return true;
-    });
-
-    const text = entry.clutter_text;
-    text.set_max_length(2);
-
-    entry.connect('key-release-event', (_: any, event: any) => {
-        const symbol = event.get_key_symbol();
-
-        const number: number | null =
-            symbol == 65293 // enter key
-                ? parse_number(text.text)
-                : symbol == 65361 // left key
-                  ? clamp(parse_number(text.text) - 1, min, max)
-                  : symbol == 65363 // right key
-                    ? clamp(parse_number(text.text) + 1, min, max)
-                    : null;
-
-        if (number !== null) {
-            text.set_text(String(number));
-        }
-    });
-
-    const create_icon = (icon_name: string) => {
-        return new St.Icon({icon_name, icon_size: 16});
-    };
-
-    entry.set_primary_icon(create_icon('value-decrease'));
-    entry.connect('primary-icon-clicked', () => {
-        text.set_text(String(clamp(parseInt(text.get_text()) - 1, min, max)));
-    });
-
-    entry.set_secondary_icon(create_icon('value-increase'));
-    entry.connect('secondary-icon-clicked', () => {
-        text.set_text(String(clamp(parseInt(text.get_text()) + 1, min, max)));
-    });
-
-    text.connect('text-changed', () => {
-        const input: string = text.get_text();
-        let parsed = parseInt(input);
-
-        if (isNaN(parsed)) {
-            text.set_text(input.substr(0, input.length - 1));
-            parsed = 0;
-        }
-
-        callback(parsed);
-    });
-
-    const item = new PopupMenuItem(label);
-    item.label.get_clutter_text().set_x_expand(true);
-    item.label.set_y_align(Clutter.ActorAlign.CENTER);
-    item.add_child(entry);
-
-    return item;
-}
-
-function parse_number(text: string): number {
-    let number = parseInt(text, 10);
-    if (isNaN(number)) {
-        number = 0;
-    }
-
-    return number;
 }
 
 function show_title(ext: Ext): any {
