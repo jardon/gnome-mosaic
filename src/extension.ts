17ab8248b6641dbf1535921f19c31b6eaa5f2859
Jarred Wilson <jarred.wilson12@gmail.com>
2025-08-29 07:34:05 -0500

    fix(hint):[#75] null borders on disable

--- src/extension.ts
+++ src/extension.ts
@@ -979,6 +979,13 @@ export class Ext extends Ecs.System<ExtEvent> {
         }
     }
 
+    destroy_all_borders() {
+        this.hide_all_borders();
+        for (const win of this.windows.values()) {
+            win.border = null;
+        }
+    }
+
     maximized_on_active_display(): boolean {
         const aws = this.workspace_id();
         for (const window of this.windows.values()) {
@@ -3036,11 +3043,11 @@ export default class MosaicExtension extends Extension {
 
         if (ext) {
             delete globalThis.MosaicExtension;
+            ext.destroy_all_borders();
             ext.injections_remove();
             ext.timeouts_remove();
             ext.signals_remove();
             ext.exit_modes();
-            ext.hide_all_borders();
             disable_window_attention_handler();
 
             layoutManager.removeChrome(ext.overlay);
